21fdab1bb6122c587d561302f10c3a87583034d4
Kulpot <kulpotwahu@gmail.com>
2023-10-06 14:18:43 +0800

    Add project files.

---
+++ Assembly Language Introduction/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+//Assembly Language Introduction
+//ref link:https://www.youtube.com/watch?v=W3roB5sRg4o&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=3
+
+//-----------------------------------------
+//Brief Look at Assembly
+//ref link:https://www.youtube.com/watch?v=Gna5eoJ0mhw&list=PLRwVmtr-pp05c1HTBj1no6Fl6C6mlxYDG&index=6
+// C++ -> CPU
